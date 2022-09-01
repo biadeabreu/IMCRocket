3bd81544dd70dfe10e8658f31d88fa5f1773f20b
Beatris Abreu <abreu.beatris@gmail.com>
2022-09-01 21:04:33 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// variaveis
+
+import {Modal} from './modal.js'
+import {AlertError} from "./alert-error.js"
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+
+
+
+
+form.onsubmit = function(event) {
+    event.preventDefault()
+
+    const weight = inputWeight.value    
+    const height = inputHeight.value
+
+    const showAlertError = notNumber(weight) || notNumber (height)
+    
+    if(showAlertError) {
+        console.log('erru')
+        AlertError.open()
+        return;
+    }
+
+    AlertError.close()
+
+    const result = IMC (weight, height)
+    const message = `Seu IMC é de ${result}`
+    
+    Modal.message.innerText = message
+    Modal.open()
+
+
+}
+
+
+
+
+function IMC (weight, height) {
+    return (weight / ((height /100) ** 2)).toFixed(2)
+}
+
+function notNumber(value) {
+    return isNaN(value) || value == ""
+}
